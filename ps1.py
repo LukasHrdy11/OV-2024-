3d8bb0b69b3a8f1b61f43c99d2cc9da68c412893
zboncakjakub <167362087+zboncakjakub@users.noreply.github.com>
2024-04-29 11:11:28 +0200

    Add files via upload

--- ps1.py
+++ ps1.py
@@ -40,10 +40,12 @@ def phases(E):
     k = math.sqrt(2*my*E)/hbarc
     rmax = 30
     h = 0.01
-    j0 = math.sin(k*(rmax-2*h))/(k*(rmax-2*h))
-    n0 = -math.cos(k*(rmax-2*h))/(k*(rmax-2*h))
-    j1 = math.sin(k*(rmax-h))/(k*(rmax-h))
-    n1 = -math.cos(k*(rmax-h))/(k*(rmax-h))
+    r2 = rmax - h
+    r1 = rmax - 2*h
+    j1 = math.sin(k * r1) / (k*r1)
+    n1 = -math.cos(k * r1) / (k * r1)
+    j2 = math.sin(k * r2) / (k*r2)
+    n2 = -math.cos(k * r2) / (k * r2)
     l = 0
 
     def numerov(E, potencial,l):
@@ -51,7 +53,6 @@ def phases(E):
             u0 = 0
             u1 = h**(l+1)
             x = int(rmax/h)
-            u = []
             u2 = 0
             for i in range (0,x-2):
                 k0 = 2*my/hbarc**2*(E - potencial(0+i*h)-hbarc**2*l*(l+1)/(2*my*rmax**2))
@@ -62,15 +63,22 @@ def phases(E):
                 u1 = u2
                 
             return u0,u1
+    
     u1, u2 = numerov(E,potencial,l)
+    b = u1/u2 * r2/r1
+    up = j1- b*j2
+    down =n1- b*n2
 
-
-
-    delta_l = math.atan((k*(rmax-h)*j0-u1/u2*k*j1)/(n0+u1/u2*n1))*180/math.pi
+    delta_l = math.atan(up/down) * 180/(math.pi)
     if delta_l < 0:
         delta_l = delta_l+180
     delta_l
     print(delta_l)
+
+
+energies1 = [0.5, 2.5, 5.0, 12.5, 25.0, 50.0, 75.0, 100.0]
+energies2 = [0.03260052732991577, 0.618570920141414, 2.809174938470539, 9.654077810188886, 19.101650821694978, 41.71985774394201, 63.480841901287825, 91.51080273814753, 119.52411262722937, 159.63028382419736, 165.5480757789632]
+energies = [0.0000000001, 10, 20, 30, 50, 73, 100, 163, 180]
 
 for energy in energy_values:
     phases(energy)
